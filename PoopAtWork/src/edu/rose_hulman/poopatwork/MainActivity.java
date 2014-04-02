13ae82a5cd5591103e36a7683809dd20e1aa2fef
Kevin Collins <kevin.collins@outlook.com>
2014-04-02 18:54:36 -0400

    back to where we were. in sync with master

--- PoopAtWork/src/edu/rose_hulman/poopatwork/MainActivity.java
+++ PoopAtWork/src/edu/rose_hulman/poopatwork/MainActivity.java
@@ -1,49 +1,18 @@
 package edu.rose_hulman.poopatwork;
 
-import java.text.NumberFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
 import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.Chronometer;
-import android.widget.Chronometer.OnChronometerTickListener;
-import android.widget.TextView;
 
 public class MainActivity extends FragmentActivity implements ActionBar.TabListener{
-
-	Button mStartButton, mStopButton;
-	Chronometer mChronometer;
-	TextView mAmountEarnedText;
-	
-	SharedPreferences mPrefs;
-	SharedPreferences.Editor mPrefsEditor;
-	
-	int salary;
-	double secondsElapsed;	
-	double amountEarned;
-	
-	String date;
-	
-	NumberFormat baseFormat;
-	
-	PoopDatabaseHandler db;
-	
 	Context context = this;
 	
 	ArrayAdapter<String> listAdapter;
@@ -59,28 +28,13 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
         viewPager = (ViewPager) findViewById(R.id.pager);
         actionBar = getActionBar();
         mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
- //testing
+ 
         viewPager.setAdapter(mAdapter);
         actionBar.setHomeButtonEnabled(false);
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);        
         actionBar.addTab(actionBar.newTab().setText("Home").setTabListener(this));
         actionBar.addTab(actionBar.newTab().setText("New Poop").setTabListener(this));
         actionBar.addTab(actionBar.newTab().setText("History").setTabListener(this));
-
-        mStartButton = (Button) findViewById(R.id.startButton);
-        mStopButton = (Button) findViewById(R.id.stopButton);
-        mChronometer = (Chronometer) findViewById(R.id.chronometer);
-        
-        mStartButton.setOnClickListener(mStartListener);
-        mStopButton.setOnClickListener(mStopListener);
-        mChronometer.setOnChronometerTickListener(mChronometerTickListener);
-		
-		db = new PoopDatabaseHandler(this);
-		
-		amountEarned = 0;
-		secondsElapsed = 0;
-		
-		baseFormat = NumberFormat.getCurrencyInstance(Locale.US);
 		
 	    viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
 	    	 
@@ -99,16 +53,10 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 	        public void onPageScrollStateChanged(int arg0) {
 	        }
 	    });
-	    
-	  
-		//This is all you need:
-//		SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
-//		String syncConnPref = sharedPref.getString(SettingsActivity.KEY_PREF_SYNC_CONN, "");
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main_activity_actions, menu);
 		return super.onCreateOptionsMenu(menu);
 	}
@@ -128,43 +76,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 		Intent i = new Intent(this, SettingsActivity.class);
 		startActivity(i);
 	}
-
-	//Listeners
-	View.OnClickListener mStopListener = new OnClickListener(){
-		public void onClick (View v){
-			mChronometer.stop();
-			date = getDateTime();			
-			Poop poop = new Poop(date, (int) secondsElapsed, (int) amountEarned);
-			db.addPoop(poop);
-		}
-	};
-
-	
-	View.OnClickListener mStartListener = new OnClickListener(){
-		public void onClick (View v){
-			salary = Integer.valueOf(mPrefs.getString("pref_wage", "0"));
-			mChronometer.setBase(SystemClock.elapsedRealtime());
-			mChronometer.start();
-		}
-	};
-	
-	OnChronometerTickListener mChronometerTickListener = new OnChronometerTickListener(){
-		@Override
-		public void onChronometerTick(Chronometer mChronometer) {
-			secondsElapsed ++;
-			amountEarned = (secondsElapsed * salary/(2080*60*60));
-			mAmountEarnedText.setText(baseFormat.format(amountEarned));
-		}
-	};
-	
-	//Helper Methods
-	private String getDateTime() {
-        SimpleDateFormat dateFormat = new SimpleDateFormat(
-                "yyyy-MM-dd HH:mm:ss", Locale.getDefault());
-        Date date = new Date();
-        return dateFormat.format(date);
-	}
-   
+ 
 	@Override
     public void onTabReselected(Tab tab, FragmentTransaction ft) {
     }
--- PoopAtWork/src/edu/rose_hulman/poopatwork/NewPoopFragment.java
+++ PoopAtWork/src/edu/rose_hulman/poopatwork/NewPoopFragment.java
@@ -1,19 +1,127 @@
 package edu.rose_hulman.poopatwork;
 
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import edu.rose_hulman.poopatwork.R.id;
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.Chronometer.OnChronometerTickListener;
+import android.widget.TextView;
+import android.widget.Toast;
  
-public class NewPoopFragment extends Fragment {
- 
+
+public class NewPoopFragment extends Fragment implements OnClickListener{
+	Button mStartButton, mStopButton;
+	Chronometer mChronometer;
+	TextView mAmountEarnedText;
+	
+
+	SharedPreferences mPrefs;
+	SharedPreferences.Editor mPrefsEditor;
+	
+	int salary;
+	double secondsElapsed;	
+	double amountEarned;
+	String date;
+	
+	NumberFormat baseFormat;
+	
+	PoopDatabaseHandler db;
+	
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
  
         View rootView = inflater.inflate(R.layout.fragment_newpoop, container, false);
-         
+        
+        db = new PoopDatabaseHandler(this.getActivity());
+        mPrefs = PreferenceManager.getDefaultSharedPreferences(this.getActivity());
+        salary = Integer.valueOf(mPrefs.getString("pref_wage", "0"));
+		amountEarned = 0;
+		secondsElapsed = 0;
+		
+		
+		baseFormat = NumberFormat.getCurrencyInstance(Locale.US);
+
+		mStartButton = (Button) rootView.findViewById(R.id.startButton);
+		mStopButton = (Button) rootView.findViewById(R.id.stopButton);
+		mChronometer = (Chronometer) rootView.findViewById(R.id.chronometer);
+		mAmountEarnedText = (TextView) rootView.findViewById(R.id.amountEarned);
+		
+		mStartButton.setOnClickListener(this);
+		mStopButton.setOnClickListener(this);
+		mChronometer.setOnChronometerTickListener(mChronometerTickListener);
+		
+//		mStartButton.setOnClickListener(new OnClickListener(){
+//			public void onClick (View v){
+//			//	int salary = Integer.valueOf(NewPoopFragment.this.mPrefs.getString("pref_wage", "0"));
+//			//	mChronometer.setBase(SystemClock.elapsedRealtime());
+//			//	mChronometer.start();
+//			}
+//		});
+		//Toast.makeText(getActivity(),"made it here", Toast.LENGTH_SHORT).show();
         return rootView;
     }
+    
+    
+    private String getDateTime() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(
+                "yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        Date date = new Date();
+        return dateFormat.format(date);
+	}
+    
+
+	//Listeners
+//	OnClickListener mStopListener = new OnClickListener(){
+//		public void onClick (View v){
+//			mChronometer.stop();
+//			date = getDateTime();			
+//			Poop poop = new Poop(date, (int) secondsElapsed, (int) amountEarned);
+//			db.addPoop(poop);
+//		}
+//	};
+
+	
+	
+	
+	OnChronometerTickListener mChronometerTickListener = new OnChronometerTickListener(){
+		@Override
+		public void onChronometerTick(Chronometer mChronometer) {
+			secondsElapsed ++;
+			amountEarned = (secondsElapsed * salary/(2080*60*60));
+			mAmountEarnedText.setText(baseFormat.format(amountEarned));
+		}
+	};
+
+	@Override
+	public void onClick(View v) {
+        switch (v.getId()) {
+        	case R.id.startButton:
+        		salary = Integer.valueOf(mPrefs.getString("pref_wage", "0"));
+        		mChronometer.setBase(SystemClock.elapsedRealtime());
+        		mChronometer.start();
+        		break;
+        	case R.id.stopButton:
+        		mChronometer.stop();
+    			date = getDateTime();			
+    			Poop poop = new Poop(date, (int) secondsElapsed, (int) amountEarned);
+    			db.addPoop(poop);
+    			break;
+        }	
+	}
 }
